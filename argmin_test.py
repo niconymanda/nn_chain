47e203685f0b070f875561c4f32561f706b609c4
niconymanda <131150025+niconymanda@users.noreply.github.com>
2024-05-24 19:46:26 +0000

    altered the argmin test function. works but not 100 a la original scipy

--- argmin_test.py
+++ argmin_test.py
@@ -1,123 +1,126 @@
 import numpy as np
 from aux_functions import *
 
-def test_nn_chain(mu, k = 5):
+def test_nn_chain(X, k = 5):
 
     """Calculates the NN chain algorithm w on the fly distances"""
 
     # Variable definition
-    n = len(mu)
-    Z = np.empty((n - 1, 4))
+    n = len(X)
     size = np.ones(n, dtype=np.intc)
-    
-    # Variables to store neighbors chain.
-    cluster_chain = np.ndarray(2*n-1, dtype=np.intc)
-    chain_length = 0
-    knn = [np.zeros(k, dtype=int) for _ in range(2 * n - 1)]
-    knn_dist = [np.zeros(k, dtype=float) for _ in range(2 * n - 1)]
+
+    active = {i for i, _ in enumerate(X)}
+    _active = [True for _ in X]
+
+    knn = []
+    knn_dist = []
     mapping = np.full((2*n-1), np.inf)
-    
-    # Begin
-    for l in range(n - 1):
 
-        # this sets the new starting cluster
+    cluster_chain = np.empty(0, dtype=np.intc)
+    chain_length = 0
+
+    Z = np.empty((0,4), int)
+
+    # Activate loop
+    while active:
+
+        if len(active) == 1:
+            break
+        
+        # New chain
         if chain_length == 0:
-            chain_length = 1
-            for i in range(2*n-1):
-                if size[i] > 0:
-                    cluster_chain[0] = i
+            print("STARTING NEW CHAIN")
+            i = next(iter(active))
+            cluster_chain = np.append(cluster_chain, i)
+
+            chain_length = 1 # Do i still need this variable ?
+            _dists = wrapper_ward(i, size, X)
+            _knn, _knn_dist = get_top_k(_dists, k)
+            print(type(knn))
+            knn.append(np.array(_knn))
+            knn_dist.append(np.array(_knn_dist))
+
+        while cluster_chain.size:
+            print("------------")
+            print(f"cluster_chain = {cluster_chain}, chain_length = {chain_length} ==> {cluster_chain[chain_length-1]}")
+            i = cluster_chain[chain_length - 1]
+            print(f"knn = {knn[-1]}, dists = {knn_dist[-1]}")
+
+            m = 0
+            ind = 1
+
+            for index, nn in enumerate(knn[-1]):
+                print(f"checking active of NN = {nn}")
+                if _active[nn]:
+                    m = index
+                    ind = 0
                     break
+            print(f"m  = {m}")
+            if ind:
+                _dists = wrapper_ward(i, size, X)
+                knn[-1], knn_dist[-1] = get_top_k(_dists, k)
+                print(type(knn))
 
-        # This find the minimal distance and checks whether the clusters are reciprocal
-        while True:
-            x = cluster_chain[chain_length - 1]
-            print(f"x = {x}")
-            print(f"knn of x = {knn[x], not any(knn[x])}")
-            print(f"condition of size of all knns of x = {size[knn[x]], not any(size[knn[x]])}")
-            
-            # if no knn has been calculated or no knn is active
-            if not any(knn[x]) or not any(size[knn[x]]):
-                # SciPy uses Euclidean for this
-                dist = wrapper_ward(x, size, mu)
-                knn[x], knn_dist[x] = get_top_k(dist, k)
 
-            y = knn[x][0]
-            print(f"original y = {y}")
-            
-            # if y has been merged
-            if size[y] == 0:
-                print("original y has been merged")
-                """
-                find the first unsplit element, so that we can find out with which element it has merged.
-                find out whether the order of the knn[x] has changed. 
-                if any of the merged element are last in the list, we can no longer ensure these are in the top k. These are popped.
-                """
-                split_index = np.where(size[knn[x]] > 0)[0][0]
-                print(f"split i = {split_index}")
-                merged_clusters = knn[x][:split_index] 
-                unioned_clusters = np.array(list(set(mapping[merged_clusters])), dtype=int) 
-                # SciPy uses Euclidean for this
-                unioned_clusters_dist = [ward(size[x], size[z], mu[x], mu[z]) for z in unioned_clusters]
-                
-                rest = knn[x][split_index:]
-                rest_dist = knn_dist[x][split_index:]
+            if knn[-1][:m].size:
+                _knn = np.array(list(set(mapping[knn[-1][:m]])), dtype=int)
+                print(f"there are un-updated NN: {knn[-1][:m]} => {_knn}")
+                print(f"replacing {knn, type(knn), knn[-1], type(knn[-1])}")
+                print(f"does this work ?? {np.append(_knn, knn[-1][m:])}")
+                knn[-1] = np.array(np.append(_knn, knn[-1][m:]))
+                print(type(knn))
+                knn_dist[-1] = np.array(np.append(dist_calculation(i, _knn, size, X), knn_dist[-1][m:]))
 
-                _knn = np.append(unioned_clusters, rest)
-                _dists = np.append(unioned_clusters_dist, rest_dist)
-                _argsorted_dists = np.argsort(_dists)
+            j = knn[-1][np.argmin(knn_dist[-1])]
+            print(f"i = {i}, j = {j}")
 
-                knn[x] = _knn[_argsorted_dists]
-                knn_dist[x] = _dists[_argsorted_dists]
-
-                y = knn[x][0]
-
-            print(f"knn[x] = {knn[x]}")
-            print(f"dists = {knn_dist[x]}")
-            print(f"y = {y}")
-
-            min_dist = knn_dist[x][0]
-
-            # Clusters reciprocal ?
-            if chain_length > 1 and y == cluster_chain[chain_length - 2]:
-                print(f"clusters reciprocal; merging {x} and {y}")
+            if chain_length > 1 and j == cluster_chain[chain_length - 2]:
                 break
 
-            cluster_chain[chain_length] = y
+            cluster_chain = np.append(cluster_chain, j)
             chain_length += 1
-
-            print()
-        
-        # Merge clusters x and y and pop them from stack.
+            _dists = wrapper_ward(j, size, X)
+            _knn, _knn_dist = get_top_k(_dists, k)
+            knn.append(np.array(_knn))
+            knn_dist.append(np.array(_knn_dist))
+            print(type(knn))
+        print()
+        print(f"merging {i, j}")
+        # Merging i and j
         chain_length -= 2
 
-        # This is a convention used in fastcluster.
-        if x > y:
-            x, y = y, x
+        size_xy = size[i] + size[j]
 
-        # get the original numbers of points in clusters x and y
-        size_xy = size[x] + size[y]
+        # Record the new node
+        Z = np.vstack([Z, [i, j, min(knn_dist[-1]), size_xy]])
 
-        # Record the new node.
-        Z[l, 0] = x
-        Z[l, 1] = y
-        Z[l, 2] = min_dist
-        Z[l, 3] = size_xy
-
-        xy_centroid = (size[x] * mu[x] + size[y] * mu[y] ) / ( size_xy )
-        print(f"centroid = {xy_centroid}")
-        mu = np.vstack([mu, xy_centroid])
+        ij_centroid = (size[i] * X[i] + size[j] * X[j] ) / ( size_xy )
+        X = np.vstack([X, ij_centroid])
+        
+        size[i] = 0
+        size[j] = 0
         size = np.append(size, size_xy)
 
-        size[x] = 0
-        size[y] = 0
+        new_index = len(X) - 1
 
-        new_index = len(mu) - 1
-        mapping[x] = new_index
-        mapping[y] = new_index
-        mapping = np.array([new_index if m == x or m == y else m for m in mapping])
+        mapping[i] = new_index
+        mapping[j] = new_index
+        mapping = np.array([new_index if m == i or m == j else m for m in mapping])
+        print(f"mapping = {mapping}")
 
-        knn[x] = [0] * k
-        knn[y] = [0] * k
+        active.remove(i)
+        active.remove(j)
+        active.add(new_index)
+
+        _active[i] = False
+        _active[j] = False
+        _active = np.append(_active, True)
+        print(f"active ? {_active}")
+
+        cluster_chain = cluster_chain[:-2]
+        knn = knn[:chain_length]
+        knn_dist = knn_dist[:chain_length]
+
         print()
         print()
 
--- aux_functions.py
+++ aux_functions.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-def ward(size_a, size_b, mu_a, mu_b):
-    return (size_a * size_b) / (size_a + size_b) * np.sqrt( sum ( ( mu_a - mu_b ) **2 ) )
+def ward(size_a, size_b, X_a, X_b):
+    return (size_a * size_b) / (size_a + size_b) * np.sqrt( sum ( ( X_a - X_b ) **2 ) )
 
-def wrapper_ward(x, size, mu):
+def wrapper_ward(i, size, X):
     """Calculates all distances from one point to the others"""
-    return [ward(size[x], size[y], mu[x], mu[y]) if y != x and size[y] > 0 else np.inf for y, _ in enumerate(mu)]
+    return [ward(size[i], size[j], X[i], X[j]) if j != i and size[j] > 0 else np.inf for j, _ in enumerate(X)]
 
 def get_top_k(distances, k):
     """Selects the top k of distances list and sorts these"""
@@ -14,7 +14,13 @@ def get_top_k(distances, k):
 
     # Re-sort the top k results
     sorted_indices = np.argsort([distances[i] for i in top_k])
-    top_k_sorted = top_k[sorted_indices]
-    top_k_dist_sorted = top_k_dist[sorted_indices]
+    top_k_sorted = np.array(top_k[sorted_indices])
+    top_k_dist_sorted = np.array(top_k_dist[sorted_indices])
     
     return top_k_sorted, top_k_dist_sorted
+
+def dist_calculation(i, targets, size, X):
+    if targets.size:
+        return np.array([ward(size[i], size[t], X[i], X[t]) for t in targets])
+    else:
+        return np.array([])
